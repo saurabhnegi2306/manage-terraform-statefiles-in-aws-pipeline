7a488e4b2186ac687257166b7b37df145ca78add
Saurabh Negi <100995248+saurabhnegi2306@users.noreply.github.com>
2024-06-27 14:14:49 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,13 @@
-terraform {
-  backend "s3" {
-    bucket         = "tfbackend-bucket-saurabhnegi1"
-    key            = "terraform.tfstate"
-    region         = "us-west-1"
-    dynamodb_table = "tfstate-lock"
-    encrypt        = true
+#terraform {
+#  backend "s3" {
+#    bucket         = "tfbackend-bucket-saurabhnegi1"
+#    key            = "terraform.tfstate"
+#    region         = "us-west-1"
+#    dynamodb_table = "tfstate-lock"
+#    encrypt        = true
+#  }
+#}
+
+  backend "local" {
+    path = "./terraform.tfstate"
   }
-}
